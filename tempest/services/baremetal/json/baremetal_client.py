bd71d28bbb87f3ed2ff208b8f38e13949574d7de
Roman Prykhodchenko <rprikhodchenko@mirantis.com>
2013-09-17 18:01:49 +0300

    Implemented create, delete and list
    requests for tempest BM client. Implemented create
    client actions for BM objects.
    
    Change-Id: Id758ae25a5d9e62c057f823179fcebe841d195bd

--- tempest/services/baremetal/json/baremetal_client.py
+++ tempest/services/baremetal/json/baremetal_client.py
@@ -18,26 +18,126 @@ from tempest.common import rest_client
 
 
 class BaremetalClientJSON(rest_client.RestClient):
+    """Tempest REST client for Ironic JSON API v1."""
 
-    def __init__(self, config, username, passwd, auth_url, tenant_name=None):
-        super(BaremetalClientJSON, self).__init__(config, username, passwd,
+    def __init__(self, config, username, password, auth_url, tenant_name=None):
+        super(BaremetalClientJSON, self).__init__(config, username, password,
                                                   auth_url, tenant_name)
         self.version = '1'
         self.uri_prefix = 'v%s' % self.version
 
-    def _list_request(self, resource):
-        uri = '%(prefix)s/%(resource)s' % {'prefix': self.uri_prefix,
-                                           'resource': resource}
+    def _get_uri(self, resource_name, uuid=None):
+        """
+        Get URI for a specific resource or object.
+
+        :param resource_name: The name of the REST resource, e.g., 'nodes'.
+        :param uuid: The unique identifier of an object in UUID format.
+        :return: Relative URI for the resource or object.
+
+        """
+        return '{pref}/{res}{uuid}'.format(prefix=self.uri_prefix,
+                                           resource=resource_name,
+                                           uuid='/%s' % uuid if uuid else '')
+
+    def _list_request(self, resource_name):
+        """
+        Get the list of objects of the specified type.
+
+        :param resource_name: The name of the REST resource, e.g., 'nodes'.
+        :return: A tuple with the server response and deserialized JSON list of
+                 of objects
+
+        """
+        uri = self.get_uri(resource_name)
+
         resp, body = self.get(uri, self.headers)
         body = json.loads(body)
 
         return resp, body
 
+    def _create_request(self, resource_name, object_dict):
+        """
+        Create an object of the specified type.
+
+        :param resource_name: The name of the REST resource, e.g., 'nodes'.
+        :param object_dict: A Python dict that represents an object of the
+                            specified type.
+        :return: A tuple with the server response and the deserialized created
+                 object.
+
+        """
+        body = json.dumps(object_dict)
+        uri = self._get_uri(resource_name)
+
+        resp, body = self.post(uri, headers=self.headers, body=body)
+        body = json.loads(body)
+
+        return resp, body
+
+    def _delete_request(self, resource_name, uuid):
+        """
+        Delete specified object.
+
+        :param resource_name: The name of the REST resource, e.g., 'nodes'.
+        :param uuid: The unique identifier of an object in UUID format.
+        :return: A tuple with the server response and the responce body.
+
+        """
+        uri = self._get_uri(resource_name, uuid)
+
+        resp, body = self.delete(uri, self.headers)
+        return resp, body
+
     def list_nodes(self):
+        """List all existing nodes."""
         return self._list_request('nodes')
 
     def list_chassis(self):
+        """List all existing chassis."""
         return self._list_request('chassis')
 
     def list_ports(self):
+        """List all existing ports."""
         return self._list_request('ports')
+
+    def create_node(self, **kwargs):
+        """
+        Create a baremetal node with the specified parameters.
+
+        :param arch: CPU architecture of the node. Default: x86_64.
+        :param cpus: Number of CPUs. Default: 8.
+        :param disk: Disk size. Default: 1024.
+        :param ram: Available RAM. Default: 4096.
+        :return: A tuple with the server response and the created node.
+
+        """
+        node = {'arch': kwargs.get('arch', 'x86_64'),
+                'cpus': kwargs.get('cpus', 8),
+                'disk': kwargs.get('disk', 1024),
+                'ram': kwargs.get('ram', 4096)}
+
+        return self._create_request('nodes', node)
+
+    def create_chassis(self, **kwargs):
+        """
+        Create a chassis with the specified parameters.
+
+        :param description: The description of the chassis. Default: my-chassis
+        :return: A tuple with the server response and the created chassis.
+
+        """
+        chassis = {'description': kwargs.get('description', 'my-chassis')}
+
+        return self._create_request('nodes', chassis)
+
+    def create_port(self, **kwargs):
+        """
+        Create a port with the specified parameters.
+
+        :param address: MAC address of the port. Default: 01:23:45:67:89:0A
+        :return: A tuple with the server response and the created port.
+
+        """
+        port = {'address': '01:23:45:67:89:0A'}
+
+        return self._create_request('ports', port)
